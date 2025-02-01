e4e8252346598f7ad925d7f64c13e51c340474a4
makau <makauwanyoike@gmail.com>
2025-02-01 11:59:03 +0300

    Square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,7 +7,7 @@ class square():
     width = 0
     height = 0
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
